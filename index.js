344c53d1a7ad05e06e6ffbf5b849733012796ac6
Artur Vanivskyi <vanivskyi.artur@gmail.com>
2023-05-05 12:35:38 -0400

    created userschema

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 
+import register from "./controllers/auth.js";
+
 // CONFIGURATIONS
 
 const __filename = fileURLToPath(import.meta.url);
@@ -35,6 +37,12 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+
+//ROUTES WITH FILES
+app.post("/auth/register", upload.single("picture"), register)
+
+
 
 //MONGOOSE SETUP
 
