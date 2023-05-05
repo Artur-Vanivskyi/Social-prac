70f0864c8683caf7ae73d3383374e115ab85e162
Artur Vanivskyi <vanivskyi.artur@gmail.com>
2023-05-05 19:07:27 -0400

    added post schema and create post function

--- routes/posts.js
+++ routes/posts.js
@@ -6,10 +6,12 @@ const router = express.Router();
 
 // READ
 router.get("/",verifyToken, getFeedPosts);
+router.get("/:userId/posts", verifyToken, getUserPosts);
 
 
 // UPDATE
 
+router.patch("/:id/like", verifyToken, likePost);
 
 
 
