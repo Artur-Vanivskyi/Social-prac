db60e01255831e23ed5b83e50651d41dbd6aa619
Artur Vanivskyi <vanivskyi.artur@gmail.com>
2023-05-05 14:38:10 -0400

    added login routes

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-
-import register from "./controllers/auth.js";
+import authRoutes from "./routes/auth.js";
+import {register} from "./controllers/auth.js"
 
 // CONFIGURATIONS
 
@@ -42,7 +42,8 @@ const upload = multer({ storage });
 //ROUTES WITH FILES
 app.post("/auth/register", upload.single("picture"), register)
 
-
+//ROUTES
+app.use("/auth", authRoutes);
 
 //MONGOOSE SETUP
 
